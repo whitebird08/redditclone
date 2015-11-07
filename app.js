bc468c3f3db9693dcc960993a4dd6b228f9b7c3d
Ryan Bird <ryan01bird@gmail.com>
2015-11-07 09:41:41 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var cloneApp = angular.module('clone', [])
+cloneApp.controller('cloneController', function($scope){
+  $scope.panels = [];
+
+  $scope.panel
+})
+
+//   $scope.contacts = [];    
+
+
+//   $scope.contact = function(contactName, contactEmail, contactPhone){
+//     var empty = {};
+//     empty.contactName = contactName
+//     empty.contactEmail = contactEmail
+//     empty.contactPhone = contactPhone
+// console.log($scope.contacts, "before")
+//     $scope.contacts.push(empty) 
+//     console.log($scope.contacts, "after")
+//   }
